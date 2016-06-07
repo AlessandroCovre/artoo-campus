1ba464b917baeeabbfdb67bd1da9d76ea40da6b8
Alessandro Covre <alessandro.covre.1985@gmail.com>
2016-06-07 16:24:16 +0000

    subjects init

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,21 @@
 // define app module
+   var app = angular
+        .module('artooInit',[])
+        .run(function(){
+            console.info('I run in the setter');
+        });
+    
+    angular
+        .module('artooInit')
+        .config(angular.noop)
+         //.run(function(){
+        //    
+        //})
+        .run(()=>{
+            console.info('I run in the getter');
+         });
+    
 // define a run function
+app.run(function(){
+   console.info('In run from the module in the var') 
+});

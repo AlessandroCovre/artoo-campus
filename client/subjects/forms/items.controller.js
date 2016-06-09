39d5034ddf237df0c849976141ef9c5aa3f548a5
Alessandro Covre <alessandro.covre.1985@gmail.com>
2016-06-09 11:00:22 +0000

    add exercise form

--- client/subjects/forms/items.controller.js
+++ client/subjects/forms/items.controller.js
@@ -1,3 +1,39 @@
-angular.module('artoo').controller('ItemsCtrl', function ($scope) {
-  $scope.items = [];
+angular.module('artoo').controller('ItemsCtrl', function ($scope, ItemsSrv) {
+  $scope.ItemsSrv = ItemsSrv;
+
+  var master;
+  
+  $scope.selectItem = (code) => {
+      $scope.item = angular.copy(ItemsSrv.find(code));
+      master = angular.copy($scope.item);
+  }
+  
+  $scope.selectedItem = ItemsSrv.get()[0].code;
+  $scope.selectItem($scope.selectedItem);
+  
+  /*$scope.item = {
+      name: 'Ascia bipenne',
+       code: 'it01',
+       description: 'L\'ascia bipenne è una scure a due lame, simbolo del potere minoico.',
+       image: 'http://khazalidgrungron.altervista.org/axes/05.png',
+  }*/
+  
+  $scope.insert = function (items, form){
+      ItemsSrv.add(items);
+      $scope.item = {};
+      //console.info('Send message: '+ text);
+      //$scope.justAnInput = '';
+      form.$setPristine();
+  };
+  
+  $scope.resetForm = (form) => {
+      $scope.item = angular.copy(master);
+      //form.$setPristine();
+  };
+  
+  $scope.update = (item, form) => {
+      ItemsSrv.update(item);
+      master = angular.copy(item);
+  };
+  
 });
